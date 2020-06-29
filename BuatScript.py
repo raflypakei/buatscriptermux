0bc2f4fb43ffd4c55b7a9b06103f68e5e8b24176
raflypakei <66076381+raflypakei@users.noreply.github.com>
2020-06-29 15:51:18 +0800

    Add files via upload

---
+++ BuatScript.py
@@ -0,0 +1,417 @@
+impor  os
+impor  sys
+ waktu impor
+cy  =  ' \ 033 [92m'
+cyy  =  ' \ 033 [96m'
+lg  =  ' \ 033 [92m'
+w   =  ' \ 033 [97m'
+lr  =  ' \ 033 [91m'
+yl  =  ' \ 033 [93m'
+h  =  ' \ 033 [95m'
+x   =  ' \ 033 [0m'
+os . sistem ( "hapus" )
+cetak  ""
+cetak  ""
+print ( cyy + "Subscribe Dulu chanel Rafly Pake i! *" )
+os . sistem ( "tidur 5" )
+os . sistem ( "hapus" )
+
+cetak ( h + "" "
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~ PEMBUAT SCRIPT DEFACE ~~
+~~ PENULIS: Rafly pake i ~~
+- DIBUAT: 27 JANUARI 2005 ~~
+~~ WHATSAPP: 082290232340 ~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"" " )
+os . sistem ( "tidur 2" )
+cetak ( cyy + "" )
+title  =  raw_input ( "Masukkan judul:" )
+nick  =  raw_input ( "Diretas oleh:" )
+team  =  raw_input ( "Nama team:" )
+img  =  raw_input ( "Tautan gambar:" )
+mencetak
+print ( cyy + "~~ Dibawah ini pesan halaman awal ya ~~" )
+pesan  =  raw_input ( "Pesan 1:" )
+pesan2  =  raw_input ( "Pesan 2:" )
+pesan3  =  raw_input ( "Pesan 3:" )
+pesan4  =  raw_input ( "Pesan 4:" )
+mencetak
+print ( cyy + "~~ Yang ini pesan untuk adminnya ~~" )
+kata  =  raw_input ( "Kata 1:" )
+kata2  =  raw_input ( "Kata 2:" )
+kata3  =  raw_input ( "Kata 3:" )
+greetz  =  raw_input ( "Terima kasih kepada:" )
+mencetak
+print ( cyy + "~~ Nama file (contoh: index.html) ~~" )
+namafile  =  raw_input ( "Nama file:" )
+m1  =  "" "<! DOCTYPE html>
+<html>
+<head>
+    <title> "" "
+m2  =  judul
+m3  =  "" "</title>
+    <meta charset = "UTF-8">
+    <meta name = "Author" content = "Seorang Tersakiti <3" />
+    <meta name = "copyright" content = "Penjelajah <3" />
+    <meta name = "description" content = "Situs web Dimiliki oleh Penjelajah" />
+    <link href = 'http: //fonts.googleapis.com/css? family = Islandia: 400.700' rel = 'stylesheet' type = 'text / css'>
+    <link href = 'http: //fonts.googleapis.com/css? family = Islandia: 400.700' rel = 'stylesheet' type = 'text / css'>
+    <meta property = "og: image" content = "">
+        <style type = "text / css">
+            tubuh {
+                overflow: disembunyikan;
+                background-image: url ('https://s22.postimg.org/lq5lty0gx/yh8t9em_VAe_Vw3kh1.jpg');
+                warna latar: # 000000;
+                background-repeat: no-repeat;
+                ukuran latar belakang: 100%;
+                posisi latar belakang: tengah atas;
+                margin: 0px;
+                kursor: tidak ada;
+                font-family: Islandia, sans-serif;
+            }
+            Sebuah{
+                teks-dekorasi: tidak ada;
+            }
+            h1 {
+            font-family: Islandia, sans-serif;
+            ukuran font: 90px;
+            warna: #fff;
+            margin: 0px 0px 0px;
+            }
+            h2 {
+            font-family: Islandia, sans-serif;
+            ukuran font: 40px;
+            warna: # 000;
+            margin: 0px;
+            text-shadow: 0 0 3px #fff;
+            
+            }
+            p {
+            warna: # 000;
+            ukuran font: 25px;
+            margin: 0px;
+            text-shadow: 0 0 3px #fff;
+            }
+            .fot {
+            font-family: Islandia, sans-serif;
+            ukuran font: 14px;
+            warna: #fff;
+            margin: 0px;
+            text-shadow: 0 0 3px # 000, 0px 0px 5px # 000;
+            }
+             h1 {
+            warna: # 000;
+            text-shadow: 0 0 5px #fff;
+        }
+        .greets {
+    font-family: Arial, sans-serif;
+    garis-tinggi: 24px;
+    ukuran font: 11px;
+    lebar: 50%;
+    latar belakang: # 000;
+    opacity: 0,9;
+    text-transform: huruf besar;
+    z-index: 9999;
+    batas-radius: 10px;
+    -moz-box-shadow: 1px 0px 2px # 000;
+    -webkit-box-shadow: 1px 0px 2px # 000;
+    box-shadow: 1px 0px 2px # 000;
+}
+        </style>
+    </head> 
+    <div id = "I301_html">
+<script type = "text / javascript" src = "http://code.jquery.com/jquery.min.js"> </script>
+<script type = "text / javascript"> setTimeout ("$ ('# loading'). fadeOut (5000);", 10000); </script>
+<style type = "text / css"> # loading {position: fixed; top: 0; left: 0; padding-top: 0px; warna latar: # 000; lebar: 100%; tinggi: 100%; warna: hitam; z-index: 9000; overflow: tersembunyi;} </style>
+<div id = "memuat">
+<body onload = "document.fpfocus ()" topmargin = "0" leftmargin = "0" bgcolor = "# 000000" marginheight = "0" marginwidth = "0">
+<table border = "0" cellpadding = "2" cellspacing = "0" width = "100%">
+<tbody> <tr> 
+</tr>
+<tr>
+</tr>
+</tbody> </table> 
+<font id = "ResponseData" color = "# ff99cc">
+<pre> <script type = "text / javascript">
+TypingText = fungsi (elemen, interval, kursor, finishedCallback) {
+  if ((typeof document.getElementById == "undefined") || (typeof element.innerHTML == "undefined")) {
+    this.running = true; // Jangan pernah lari.
+    kembali;
+  }
+  this.element = elemen;
+  this.finishedCallback = (finishedCallback? finishedCallback: function () {return;});
+  this.interval = (typeof interval == "undefined"? 100: interval);
+  this.origText = this.element.innerHTML;
+  this.unparsedOrigText = this.origText;
+  this.cursor = (cursor? cursor: "");
+  this.currentText = "";
+  this.currentChar = 0;
+  this.element.typingText = ini;
+  if (this.element.id == "") this.element.id = "typingtext" + TypingText.currentIndex ++;
+  TypingText.all.push (ini);
+  this.running = false;
+  this.inTag = false;
+  this.tagBuffer = "";
+  this.inHTMLEntity = false;
+  this.HTMLEntityBuffer = "";
+}
+TypingText.all = new Array ();
+TypingText.currentIndex = 0;
+TypingText.runAll = function () {
+  untuk (var i = 0; i <TypingText.all.length; i ++) TypingText.all [i] .run ();
+}
+TypingText.prototype.run = function () {
+  jika (this.running) kembali;
+  if (ketik this.origText == "undefined") {
+    setTimeout ("document.getElementById ('" + this.element.id + "') .typingText.run ()", this.interval); // Kami belum selesai memuat. Bersabarlah.
+    kembali;
+  }
+  if (this.currentText == "") this.element.innerHTML = "";
+// this.origText = this.origText.replace (/ <([^ <]) *> /, ""); // Strip HTML dari teks.
+  if (this.currentChar <this.origText.length) {
+    if (this.origText.charAt (this.currentChar) == "<" &&! this.inTag) {
+      this.tagBuffer = "<";
+      this.inTag = true;
+      this.currentChar ++;
+      this.run ();
+      kembali;
+    } lain jika (this.origText.charAt (this.currentChar) == ">" && this.inTag) {
+      this.tagBuffer + = ">";
+      this.inTag = false;
+      this.currentText + = this.tagBuffer;
+      this.currentChar ++;
+      this.run ();
+      kembali;
+    } lain jika (this.inTag) {
+      this.tagBuffer + = this.origText.charAt (this.currentChar);
+      this.currentChar ++;
+      this.run ();
+      kembali;
+    } lain jika (this.origText.charAt (this.currentChar) == "&" &&! this.inHTMLEntity) {
+      this.HTMLEntityBuffer = "&";
+      this.inHTMLEntity = true;
+      this.currentChar ++;
+      this.run ();
+      kembali;
+      } lain jika (this.origText.charAt (this.currentChar) == ";" && this.inHTMLEntity) {
+      this.HTMLEntityBuffer + = ";";
+      this.inHTMLEntity = false;
+      this.currentText + = this.HTMLEntityBuffer;
+      this.currentChar ++;
+      this.run ();
+      kembali;
+    } lain jika (this.inHTMLEntity) {
+      this.HTMLEntityBuffer + = this.origText.charAt (this.currentChar);
+      this.currentChar ++;
+      this.run ();
+      kembali;
+    } lain {
+      this.currentText + = this.origText.charAt (this.currentChar);
+    }
+    this.element.innerHTML = this.currentText;
+    this.element.innerHTML + = (this.currentChar <this.origText.length - 1? (ketik this.cursor == "function"? this.cursor (this.currentText): this.cursor): "");
+    this.currentChar ++;
+    setTimeout ("document.getElementById ('" + this.element.id + "') .typingText.run ()", this.interval);
+  } lain {
+    this.currentText = "";
+    this.currentChar = 0;
+        this.running = false;
+        this.finishedCallback ();
+  }
+}
+</script>
+<script>
+fungsi nonaktifkan pilih (e) {return false}
+function reEnable () {return true}
+// jika IE4 +
+document.onselectstart = Fungsi baru ("return false")
+// jika NS6
+if (window.sidebar) {
+document.onmousedown = disableselect
+document.onclick = reEnable
+}
+</script>
+<script>
+var message = "";
+fungsi clickIE ()
+{if (document.all)
+{(message); return false;}}
+fungsi clickNS (e) {
+jika
+(document.layers || (document.getElementById &&! document.all))
+{
+if (e.which == 2 || e.which == 3) {(message); return false;}}}
+if (document.layers)
+{document.captureEvents (Event.MOUSEDOWN); dokumen. onmousedown = clickNS;}
+lain
+{document.onmouseup = clickNS; document.oncontextmenu = clickIE;}
+document.oncontextmenu = Fungsi baru ("return false")
+</script>
+<table style = "background-repeat: no-repeat;" align = "right" border = "0" width = "100%">
+Situs
+<tbody> <tr>
+<td valign = "top"> <p id = "hack">
+<br />
+Situs
+<font color = "Hijau"> <b> ""
+m4  =  pesan
+m5  =  "" "<br> <br>
+<font color = "Hijau"> <b> ""
+m6  =  pesan2
+m7  =  "" "<br> <br>
+<font color = "Hijau"> <b> ""
+m8  =  pesan3
+m9  =  "" "<br> <br>
+<font color = "Hijau"> <b> ""
+m10  =  pesan4
+m11  =  "" "</font>
+</p> </tr>
+</tbody> </table> </div> 
+Situs
+<script type = "text / javascript">
+TypingText baru (document.getElementById ("hack"), 50, function (i) {var ar = new Array ("_", ""); return "" + ar [i.length% ar.length];}) ;
+TypingText.runAll ();
+</script>
+<style> 
+      td
+      {
+        warna latar: # 000000;
+        font-family: Courier New;
+        ukuran font: 20px;
+        warna: # 000000;
+        warna batas: # 000000;
+        lebar-perbatasan: 1pt;
+        gaya perbatasan: solid;
+        border-collapse: runtuh;
+        padding: 0pt 3pt;
+        vertical-align: top;
+      }
+      meja
+      {
+        warna perbatasan: # 88aace;
+        lebar-perbatasan: 0pt 1pt;
+        gaya perbatasan: tanda hubung;
+      }
+      A: Tautan, A: Dikunjungi
+      {
+        warna: # 88aace;
+      }
+      A.no:Link, A.no:Kunjungi
+      {
+warna: # 88aace;
+        teks-dekorasi: tidak ada;
+      }
+      A: Hover, A: Dikunjungi: Hover, A.no:Hover, A.no:Visited:Hover
+      {
+      warna: # 88aace;
+        warna latar: # 2e2e2e;
+        teks-dekorasi:
+        garis bawahi garis bawah;
+      }
+      .style1
+      {
+        warna: # 88aace
+      }
+      .style2
+      {
+        warna: 1f1f1f
+      }
+      tubuh
+      {
+        warna putih;
+         
+        posisi latar belakang: kanan;
+        background-attachment: fix;
+        </div>
+      }
+    </style>
+        
+</div>
+</div>
+<body oncontextmenu = "return false" onkeydown = "return false">
+<center>
+<h2 class = "glow"> ""
+m12  =  tim
+m13  =  "" "</h2>
+<img src = "" "
+t1  =  img
+t2  =  "" "width =" 250 "height =" 250 ">
+<h2 class = "glow2">. :: Diretas Oleh ::. <br> <span style = "color: # 000; font-family: Iceland; text-shadow: SkyBlue 0px 0px 10px"> ./ </ span > <span style = "color: # 000; font-family: Iceland; text-shadow: red 0px 0px 10px"> "" "
+t3  =  nick
+t4  =  "" "</span> </b> </h2>
+<p> <b> <font color = "biru"> "
+t5  =  kata
+t6  =  "" ". </font> <span style =" font-family: Iceland; color: red; text-shadow: # 000 0px 0px 3px ">" ""
+t7  =  kata2
+t8  =  "" ". </span> <font color =" biru ">" ""
+tt  =  kata3
+t9  =  "" "</font> <font color =" color: red; text-shadow: # 000 0px
+0px 3px "> </font> <br> # * # * # * #
+            </p>
+        </span>
+            <div style = "ukuran font: 10px; warna: emas; bayangan teks: abu-abu 0px 0px 3px">
+        <span style = "font-family: Iceland; font-weight: bold; color: #ffffff"> <p> ~ "" "
+t10  =  tim
+t11  =  "" "~ </p> </span>
+    </div>
+<div class = "salam">
+<table align = batas tengah = "0">
+<tr>
+<td width = 100% id = CROOTZ>
+<marquee behaviour = "scroll" direction = "left" scrollamount = "4" scrolldelay = "55" width = "100%">
+<font size = "5px" style = "font-family: Iceland, sans-serif; warna: hitam; teks-bayangan: 0 0 3px merah, 0px 0px 5px merah">
+<b> - = | "" "
+T12  =  greetz
+t13  =  "" "= - </font>
+</marquee>
+</td>
+</table> </div> 
+<div class = "fot">
+DUNIA HACKER INDONESIA [+] ANONYMOUS INDONESIA [+] "" "
+l1  =  tim
+l2  =  "" "</div>
+</center>
+</body>
+<object data = "http://flash-mp3-player.net/medias/player_mp3.swf" width = "0" height = "0" type = "application / x-shockwave-flash"> <param value = " #ffffff "name =" bgcolor "> <param value =" mp3 = http: //zizka.free.fr/_muisc/04-kanye_west-amazing_ft._young_jeezy- (HHKingz.net) .mp3 & loop = 1 & autoplay = 1 & volume = 125 " name = "FlashVars"> </object>
+</body>
+</html> "" "
+file  =  terbuka ( namafile , "w" )
+file . tulis ( m1 )
+file . tulis ( m2 )
+file . tulis ( m3 )
+file . tulis ( m4 )
+file . tulis ( m5 )
+file . tulis ( m6 )
+file . tulis ( m7 )
+file . tulis ( m8 )
+file . tulis ( m9 )
+file . tulis ( m10 )
+file . tulis ( m11 )
+file . tulis ( m12 )
+file . tulis ( m13 )
+file . tulis ( t1 )
+file . tulis ( t2 )
+file . tulis ( t3 )
+file . tulis ( t4 )
+file . tulis ( t5 )
+file . tulis ( t6 )
+file . tulis ( t7 )
+file . tulis ( t8 )
+file . tulis ( tt )
+file . tulis ( t9 )
+file . tulis ( t10 )
+file . tulis ( t11 )
+file . tulis ( t12 )
+file . tulis ( t13 )
+file . tulis ( l1 )
+file . tulis ( l2 )
+os . sistem ( "hapus" )
+mencetak
+cetak  "~~ SCRIPT BERHASIL DIBUAT ~~"
+print  "~~ LIHAT SCRIPT ANDA BERNAMA" , namafile
+cetak  "~~ TERIMA KASIH SUDAH MEMAKAI TOOL SAYA ~~"
+mencetak
+cetak  "BYE ... BYE ..."
+
+file . tutup ()
